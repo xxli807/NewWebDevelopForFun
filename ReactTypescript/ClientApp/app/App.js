1dd5956222710feeca7cdd9f8260742db6d10251
lxx871030@gmail.com <lxx871030@gmail.com>
2017-06-13 21:38:27 +1000

    fix the eslint error accordingly

--- ReactTypescript/ClientApp/app/App.js
+++ ReactTypescript/ClientApp/app/App.js
@@ -2,10 +2,10 @@
 import React from 'react';
 
 const App = () => (
-    <div>
-        <h2>User List</h2>
-        <h1>test hot reload</h1>
-    </div>
+  <div>
+    <h2>User List</h2>
+    <h1>test hot reload</h1>
+  </div>
 );
 
 export default App;
